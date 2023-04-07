ac66148c5f04d4b20401517fece2d0b1eeb085f0
Khanh Tran <katetrannbkhanh@gmail.com>
2023-04-06 18:35:33 -0700

    more components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import Header from './Header'
 import ProductPage from './ProductPage';
 import NavBar from './NavBar';
+import Brands from './Brands';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NavBar />
-      <ProductPage />
+      <Switch>
+          <Route path="/Brands">
+            <Brands />
+          </Route>
+          <Route exact path="/">
+            <ProductPage />
+          </Route>
+        </Switch>
+
     </div>
   );
 }
