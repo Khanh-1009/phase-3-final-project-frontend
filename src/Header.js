438a28b49a37bac131ba9951ba21ab1bd6c8c3b9
Khanh Tran <katetrannbkhanh@gmail.com>
2023-04-06 17:28:55 -0700

    Adding components

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        SKINWISH - <i>explore Korean Beauty</i>
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/004/750/303/small/love-nature-with-beauty-woman-face-free-vector.jpg"/>
+        
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
